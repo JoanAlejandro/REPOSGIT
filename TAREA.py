d3e27d7caa8e558fe57cee864ce4a842d4e9f2f2
jquinteroov <jquinteroov@unal.edu.co>
2024-11-27 20:28:41 -0500

    parte 2

--- TAREA.py
+++ TAREA.py
@@ -4,3 +4,12 @@ V=int(input("Cuantas vacas tiene el corral "))
 litro_dia=M/A
 Total=(V*litro_dia)*7
 print(f"{V} Vacas producen {Total} litros de leche ")
+
+
+aves=int(input("Cuantas aves tiene el galpon "))
+gall=aves/3
+mg=gall/2
+mg1=mg*(30/3)
+mg2=mg*(30/5)
+mgt=mg1+mg2
+print(f"en un mes producen {mgt} huevos mensuales")
